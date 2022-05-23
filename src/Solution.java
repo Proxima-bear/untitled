55828ac15ee38877065456d312b9f85753611cc3
Dmitry Palkin <a9122496136@gmail.com>
2022-05-23 18:41:46 +0500

    -review

--- src/Solution.java
+++ src/Solution.java
@@ -55,42 +55,73 @@ public class Solution {
 //        System.out.println(x); // the end of the first task
 
 
-        // the task number 8.11 time 15:04
-//        System.out.println("enter amount of students");
-//        int a = reader.nextInt();
-//        System.out.println("enter amount if chairs");
-//        int b = reader.nextInt();
-//        if (a>b) {
-//            a = a - b;
-//            System.out.println(a);
+        /**
+         * 1.
+         * Лучше в таких задачах называть переменные более понятным образом (См задачу)
+         * 2.
+         * В случае, когда студентов больше стульев - программа работает некорректно.
+         */
+//
+//        System.out.println("Enter amount of students.");
+//        int amountOfStudents = reader.nextInt();
+//
+//        System.out.println("Enter amount if chairs.");
+//        int amountOfChairs = reader.nextInt();
+
+
+        /**
+         * Обрати внимание, что после того, как мы переименовали переменные, в нижних фрагментах кода получилось нечто некрасивое
+         * А именно, мы перезаписываем переменную amountOfChairs количеством стульев, которых не хватает. Логично было бы создать для этого
+         * другую переменную, что бы не вгонять читающего в ступор.
+         */
+//        int chairsOverlap = 0;
+//
+//        if (amountOfStudents > amountOfChairs) {
+//            amountOfStudents = amountOfStudents - amountOfChairs;
+//            System.out.println(amountOfStudents);
 //            System.out.println("стульев не хватает");
 //        }
-//        if (a<b) {
-//            b = b - a;
-//            System.out.println(b);
+//        if (amountOfStudents < amountOfChairs) {
+//            amountOfChairs = amountOfChairs - amountOfStudents;
+//            System.out.println(amountOfChairs);
 //            System.out.println("дима лох, а стульев многовато");
 //        }
-//        if (a==b)
+//        if (amountOfStudents == amountOfChairs)
 //            System.out.println("всего хватает, а дмитрий смотрит хентай)"); // the end of the task 8.11 time 15:38
 
 
         // task 8.12 time 15:42
+        /**
+         * 404 - HTTP ошибка, свидетельствующая о том, что нужный нам веб ресурс не был найден =)
+         */
 //        int a = reader.nextInt();
 //        if (a < 1 || a > 5) {
 //            System.out.println("error:404 иди учись добоебина Ёбаная ");
 //        }
 //        else {
 //            System.out.println("мужчина");
-//        }                                     // the end of the task 8.12
+//        }
 
 
         // task 8.14 time 15:58
+        /**
+         * Если условие не проходит - ничего не происходит
+         */
 //        int a = reader.nextInt();
 //        int b = a/10;
 //        int c = a%10;
 //        if (b > c)
-//            System.out.println(true); // the end of the task 16:02
+//            System.out.println(true);
+
+
+
         // task 8.31 time 16:09
+
+        /**
+         * В задаче сказано, что в случае с нечетным числом - все выполняется наоборот. Значит, в случае с числом 999
+         * мы должны оставить его таким, какое оно есть, так как мы не можем прибавить к 9 единицу по условию задачи.
+         * Протестируй со всеми возможными значениями такую задачу лучше.
+         */
         int a = reader.nextInt();
         int b = a / 100;
         int c = a%100/10;
@@ -107,8 +138,7 @@ public class Solution {
                 d = d + 1;
             else
                 d = d - 1;
-        System.out.println(b * 100 + c*100/10 + d); // task 8.31 complete time 17:40
-
+        System.out.println(b * 100 + c*100/10 + d);
 
     }
 }
